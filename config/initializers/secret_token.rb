f5b2238d59be144e4b33e3263e761eaa6b87b684
Daniel Mahlow <dmahlow@gmail.com>
2013-08-17 18:08:52 +0200

    Add rails4.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Things::Application.config.secret_token = '9f9a02c84ba9ea42b1dfa967a5364397a3ac0be2020be1efbc47d72f3230687d013dcab7f8378e3b71a14223bcb30da4fa21d6d80e81c5746f72f06c14c78298'
